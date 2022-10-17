4b8e73d2058ef248e10b9cecd56d5f6aa660667c
Riya Cherlakola <riyac4@illinois.edu>
2022-10-16 20:50:33 -0500

    Created nav-bar with MUI

---
+++ sail-website/src/components/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      {/* <Routes> */}
+        {/* <Route path="/Home" element={<Home />} /> */}
+        {/* <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Spotify" element={<Spotify />} /> */}
+        {/* <Route path="/Login" element={<Login />} /> */}
+      {/* </Routes> */}
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter
