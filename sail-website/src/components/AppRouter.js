72db4e2563188d2ba3d28f9dab9327d1abfc8a29
Riya Cherlakola <riyac4@illinois.edu>
2022-11-28 18:29:14 -0600

    Moved it out of App.js

--- sail-website/src/components/AppRouter.js
+++ sail-website/src/components/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
-import Home from './Home'
+import Home from './HomePage'
 
 function AppRouter() {
   return (
