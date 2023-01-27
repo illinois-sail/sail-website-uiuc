d684dc5b08f91baf09d255c511e19cd57b6ad0d5
riya-v-23 <riyarverma23@gmail.com>
2023-01-27 17:59:36 -0600

    fixed layout.js error

--- sail-website/src/App.js
+++ sail-website/src/App.js
@@ -1,33 +1,31 @@
-import './App.css';
-import AppRouter from './components/AppRouter';
-import NavBar from './components/NavBar';
+import "./App.css";
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
 //import React from '/react';
-//import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
-import HomePage from './components/HomePage';
-import Home from './components/Home';
-import Typography from '@mui/material/Typography';
-import Question from './components/Questions';
+//import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import HomePage from "./components/HomePage";
+import Home from "./components/Home";
+import Typography from "@mui/material/Typography";
+import Question from "./components/Questions";
 import Box from "@mui/material/Box";
-import beachHut from './surf_hut.png'
+import beachHut from "./surf_hut.png";
 import React from "react";
 import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import ResetPassword from "./components/ResetPassword";
 import ResetPasswordConfirm from "./components/ResetPasswordConfirm";
-import Activate from "./components/Activate"
-import {Provider} from 'react-redux';
-import store from './components/store';
-import Layout from './components/layout';  
-
-
+import Activate from "./components/Activate";
+import { Provider } from "react-redux";
+import store from "./components/store";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div>
-       <AppRouter />
-       {/* <Provider store ={store}>         */}
-       {/* <Router>
+      <AppRouter />
+      {/* <Provider store ={store}>         */}
+      {/* <Router>
           <Layout>
             <Routes>
               <Route exact path = '/' element={HomePage}/>
@@ -40,9 +38,9 @@ function App() {
           </Layout>
         </Router> */}
       {/* </Provider> */}
-       <NavBar />
-       {/* <HomePage /> */}
-       {/* <div className="homepage">
+      <NavBar />
+      {/* <HomePage /> */}
+      {/* <div className="homepage">
        <div>
         <br></br>
        </div>
@@ -84,10 +82,7 @@ function App() {
             </Box>
       
       </div> */}
-      
-    </div> 
-       
-    
+    </div>
   );
 }
 
