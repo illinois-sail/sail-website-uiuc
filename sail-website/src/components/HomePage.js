543cd1f7c4d397106d737d97849edcdc0f1b0e6a
Akanksha Chablani <ac82@illinois.edu>
2023-03-05 14:11:18 -0600

    different approaches for a responsive homepage

--- sail-website/src/components/HomePage.js
+++ sail-website/src/components/HomePage.js
@@ -2,8 +2,11 @@ import React from 'react';
 // import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Home from './Home';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+// import {createTheme, ThemeProvider} from '@mui/material/styles';
 // import { width } from '@mui/system';
+import mobile from './Mobile.png';
+import tablet from './Tablet.png';
+import desktop from './Desktop.png';
 
 
 // const styles = ({
@@ -16,60 +19,67 @@ import {createTheme, ThemeProvider} from '@mui/material/styles';
 //     }
 // });
 
-const theme = createTheme({
-    breakpoints: {
-        values: {
-            mobile: 0,
-            tablet: 768,
-            laptop: 1024,
-            desktop: 1280,
-        },
-    },
-});
+// const theme = createTheme({
+//     breakpoints: {
+//         values: {
+//             mobile: 0,
+//             tablet: 768,
+//             laptop: 1024,
+//             desktop: 1280,
+//         },
+//     },
+// });
 
 function HomePage() {
             // <Paper style={styles.paperContainer}>
             // </Paper>
             return (
-                <ThemeProvider theme={theme}>
+                // <ThemeProvider theme={theme}>
             <Box
+            
                 sx={{
-                    backgroundImage: `url(${"https://raw.githubusercontent.com/illinois-sail/sail-website-uiuc/fd91ec28aef31ddc27c219a6e000190e8bff2eed/sail-website/src/landing_with_title.svg"})`,
-                    backgroundSize: 'cover',
+                    backgroundImage: {
+                        xs: 'url("https://raw.githubusercontent.com/illinois-sail/sail-website-uiuc/ac-homepage/sail-website/src/Mobile.png")',
+                        md: 'url("https://raw.githubusercontent.com/illinois-sail/sail-website-uiuc/ac-homepage/sail-website/src/Tablet.png")',
+                        lg: 'url("https://raw.githubusercontent.com/illinois-sail/sail-website-uiuc/ac-homepage/sail-website/src/Desktop.png")',
+                      },
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: 'center center',
+                      height: '100vh',
                     // backgroundColor: '#C0FDFB',
                     // resizeMode: "stretch",
                     // margin: 10,
                     // width: '100%',
-                    // minHeight: '100vh',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center center',
+                    // // minHeight: '100vh',
+                    // backgroundRepeat: 'no-repeat',
+                    // backgroundPosition: 'center center',
                     // backgroundAttachment: 'fixed',
                     // flexGrow: 1,
                     // overflow: 'hidden',
                     // maxWidth: '100%',
                     // height: 'auto',
-                height: {
-                    mobile: '100px',
-                    tablet: '500px',
-                    laptop: '1000px',
-                    desktop: '1000px',
-                },
-                // width: '7680px',
-                animation: 'slide 3s linear infinite',
-                width: {
-                    mobile: '100%',
-                    tablet: '100%',
-                    laptop: '100%',
-                    desktop: '100%',
-                }
+                // height: {
+                //     mobile: '100px',
+                //     tablet: '500px',
+                //     laptop: '1000px',
+                //     desktop: '1000px',
+                // },
+                // // width: '7680px',
+                // animation: 'slide 3s linear infinite',
+                // width: {
+                //     mobile: '100%',
+                //     tablet: '100%',
+                //     laptop: '100%',
+                //     desktop: '100%',
+                // }
             }}
             
             >
             <Home />
             </Box>
-</ThemeProvider>
             
-            );
+        );
 }
 
 export default HomePage;
