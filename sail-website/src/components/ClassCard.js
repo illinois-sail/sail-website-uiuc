22ada7f94d103dc9423c9146569fdfd8059ac588
riya-v-23 <riyarverma23@gmail.com>
2023-03-31 10:08:01 -0500

    added a classes page

---
+++ sail-website/src/components/ClassCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import "./ClassCard.css";
+const ClassCard = (props) => {
+  return (
+    <Card className="card-style">
+      <CardActionArea>
+        <CardContent>
+          <Typography
+            variant="h6"
+            className="title" 
+          >
+            {props.title}
+          </Typography>
+          <Typography
+            variant="h6"
+            className="teacher" 
+          >
+            {props.teacher}
+          </Typography>
+          <Typography
+            variant="body4"
+            className="desc"
+          >
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ClassCard;
