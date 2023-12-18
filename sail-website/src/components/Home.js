e8dd47ec6831b7c525c41a73efcb898ab55a5e74
Sanjay Manoj <sanjaymanoj04@gmail.com>
2023-12-17 21:25:30 -0600

    static navbar, center text change, spacing on the home page

--- sail-website/src/components/Home.js
+++ sail-website/src/components/Home.js
@@ -4,32 +4,6 @@ import "../fonts.css";
 import VideoPlayer from './VideoPlayer';
 import HomePageTitle from "./HomePageTitle";
 
-
-// const schedule_day_1 = [
-//   { time: "8:00 AM", event: "Check-in and Breakfast" },
-//   { time: "9:00 AM", event: "Opening Ceremony" },
-//   { time: "9:30 AM", event: "Class #1" },
-//   { time: "10:30 AM", event: "Class #2" },
-//   { time: "11:30 PM", event: "RSO fair" },
-//   { time: "12:00 PM", event: "Lunch" },
-//   { time: "1:00 PM", event: "Class #3" },
-//   { time: "2:00 PM", event: "Engineering Campus Tour" },
-//   { time: "3:00 PM", event: "Prospective Student Q&A Panel" },
-//   { time: "3:00 PM", event: "Admitted Student Q&A Panel"}, 
-//   { time: "4:00 PM", event: "Professor Wade Talk"}, 
-//   { time: "5:00 PM", event: "Closing Ceremony"}
-// ];
-
-// const schedule_day_2 = [
-//   { time: "12:00 PM", event: "Opening Ceremony"},
-//   { time: "12:30 PM", event: "Hackathon Starts"},
-//   { time: "2:30 PM", event: "Submissions due"},
-//   { time: "2:30 PM", event: "Q & A or Colleen talk"},
-//   { time: "3:00 PM", event: "Present Top 3 Submissions"},
-//   { time: "3:30 PM", event: "5 mins to vote for best submission"},
-//   { time: "3:35 PM", event: "Present winners and prizes"}
-// ]
-
 const Home = () => {
   return (
     <div className="homepage">
--- sail-website/src/components/NavBar.js
+++ sail-website/src/components/NavBar.js
@@ -1,60 +1,29 @@
 import * as React from "react";
-import logo from "../assets/sail24logo.png";
+import logo from "../assets/sail_logo_transparent.png";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
 import "../fonts.css";
-import { useMediaQuery } from "@material-ui/core";
 import CyberButton from "./CyberButton";
-import sail_logo_transparent from "../assets/sail_logo_transparent.png";
 
-const drawerWidth = 240;
 const navItems = ["register", "classes", "hackathon", "about"];
+
 function NavBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   return (
     <Box sx={{ display: "block", boxShadow: "0" }} >
-      <AppBar component="nav" sx={{ backgroundColor: "transparent" }} style={{paddingLeft: "20px", paddingTop: "50px", boxShadow: "none"}}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 1, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
+      {/* <AppBar component="nav" sx={{ backgroundColor: "transparent" }} style={{paddingLeft: "20px", paddingTop: "50px", boxShadow: "none"}}> */}
+        <Toolbar sx = {{ marginTop: "50px" }}>
           <Typography
             variant="h6"
             component="div"
             sx={{ flexGrow: 1, fontFamily: "sergiotrendy" }}
           >
-            <Button 
-              href = "/"
-              backgroundImg = {logo}
-            >
-              {/* add in the logo here as an image */}
-              <img src={sail_logo_transparent} style={{width: "120px"}} alt="top_left_logo"/>
-            </Button>
+            <a href="/">
+              <img src={logo} style={{ width: "80px", height: "80px"}} alt="top_left_logo"/>
+            </a>
+
           </Typography>
           <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row', alignItems: 'center' }}>
             {navItems.map((item, index) => (
@@ -62,7 +31,7 @@ function NavBar(props) {
             ))}
           </Box>
         </Toolbar>
-      </AppBar>
+      {/* </AppBar> */}
     </Box>
   );
 }
