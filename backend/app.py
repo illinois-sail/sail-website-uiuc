fba865ace996197e06451142824fa59daf2cffd6
Adrian Szwejkowski <77693969+adrian-szwejk@users.noreply.github.com>
2024-02-07 15:13:44 -0600

    fixed issue with {} in SQL cmd not having '' outside of them

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import sqlite3
 import hashlib
 from hash_password import hash_password
 
-load_dotenv()
+
+
+#load_dotenv()
 
 app = Flask(__name__, static_url_path='/', static_folder='../frontend/build', template_folder='../frontend/build')
 
+if __name__ == '__main__':
+    app.run(debug=True)
+    # Call your routes here to test them
+    app.test_client().post('/signup', json={'first_name': 'John', 'last_name': 'Doe', 'email': 'johndoe@gmail.com', 'password': 'password', 'shirt_size': 'M', 'parent_name': 'Jane Doe', 'parent_email': 'janedoe@gmail.com'})
+    print(app.test_client().get('/get-students').data)
+    app.test_client().post('/login', json={'email': 'johndoe@gmail.com', 'password': 'password'})
+    print(app.test_client().get('/get-students').data)
+    app.test_client().post('/change_password', json={'email': 'johndoe@gmail.com', 'old_password': 'password', 'new_password': 'test'})
+    print(app.test_client().get('/get-students').data)
+    
 index = 1
 
 # define a connection and cursor
@@ -38,7 +49,17 @@ cursor.execute(f"INSERT INTO student_accounts VALUES {test_student2}")
 
 cursor.execute("SELECT * FROM student_accounts")
 rows = cursor.fetchall()
+print("------------------------Before------------------------")
 print(rows)
+
+#Change password for test_student1
+print("------------------------After------------------------")
+cursor.execute(f"UPDATE student_accounts SET password_hash = '{hash_password('newpassword')}' WHERE email = 'testemail@gmail.com' AND password_hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'")
+cursor.execute("SELECT * FROM student_accounts")
+rows = cursor.fetchall()
+print(rows)
+
+
 
 @app.route('/')
 def index():
@@ -52,7 +73,7 @@ def classes():
 def login_page():
     return render_template('index.html')
 
-@app.route('change_password', methods=['POST'])
+@app.route('/change_password', methods=['POST'])
 def change_password():
     response = request.json
     email = response['email']
@@ -66,11 +87,12 @@ def change_password():
     print(rows)
 
     for row in rows:
+        #Check if email and password match before changing
         if row[1] == email and row[2] == hashed_old_password:
-            cursor.execute(f"UPDATE student_accounts SET password_hash = {hashed_new_password} WHERE email = {email}")
+            cursor.execute(f"UPDATE student_accounts SET password_hash = '{hashed_new_password}' WHERE email = '{email}'")
             return "Password changed successfully", 200
         
-    return "Invalid New Password", 400
+    return "Invalid Email or Password", 400
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -100,7 +122,12 @@ def signup():
     shirt_size = response['shirt_size']
     parent_name = response['parent_name']
     parent_email = response['parent_email']
+    print('Received:', first_name, last_name, email, password, shirt_size, parent_name, parent_email)
     
+    cursor.execute("SELECT * FROM student_accounts")
+    rows = cursor.fetchall()
+    print("------------------------Test------------------------")
+    print(rows)
     # TODO: Add the user to the database
     student = (index, email, hash_password(password), first_name, last_name, shirt_size, parent_name, parent_email)
     cursor.execute(f"INSERT INTO student_accounts VALUES {student}")
