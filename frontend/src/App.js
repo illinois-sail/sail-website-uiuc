976b828a8514e7eefe37f46b54ec0be0e4ad8ce0
tiakashyap <tia.kashyap.2005@gmail.com>
2024-11-09 18:11:19 -0600

    Deleted frontend
    Deleted frontend, created new React app for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,24 @@
-import React, { useEffect } from "react";
+import logo from './logo.svg';
 import './App.css';
-import AppRouter from './components/AppRouter';
-import NavBar from './components/NavBar';
-import background from "./assets/final_background.png";
-import { AuthProvider } from "./components/AuthContext";
-import Footer from './components/Footer'; // Import the Footer component
-const App = () => {
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const newBackgroundSize = 100 + scrollPosition * 0.1; // Adjust the multiplier as needed
-      document.getElementById("background-container").style.backgroundSize = `${newBackgroundSize}% ${newBackgroundSize}%`;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
+function App() {
   return (
-      <AuthProvider>
-        <div style={{ position: "relative", minHeight: "100vh" }}>
-        <div
-          id="background-container"
-          className="background-container"
-          style={{
-            backgroundImage: `url(${background})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundAttachment: "fixed",
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: -1,
-            transition: "background-color 0.3s ease", // Add transition property for smooth color change
-          }}
-        />
-          <div style={{ zIndex: 1 }}>
-            <NavBar />
-            <AppRouter />
-          </div>
-          <div style={{position: "absolute", bottom: 40, width: "100%"}}>
-            {/* <Footer style={{  }} /> */}
-          </div>
-        </div>
-      </AuthProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CS SAIL
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- frontend/src/components/VideoPlayer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import video from './promo.mp4'
-
-function VideoPlayer() {
-  return (
-    <video style={{ width: "80%", height: "80%", objectFit: "cover" }} src = {video} controls/>
-  );
-}
-export default VideoPlayer;
