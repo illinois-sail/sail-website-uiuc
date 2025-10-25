5cd5a4209ee9908f3b35a3f1950b5f98821661c0
tiakashyap <tia.kashyap.2005@gmail.com>
2025-10-25 12:02:47 -0500

    remove frontend for 2026 base setup

--- frontend/src/components/server_url.js
+++
@@ -1,8 +0,0 @@
-const PROD_SERVER = "https://sail.cs.illinois.edu";
-const TEST_SERVER = "http://10.192.104.68:5000";
-
-// server URL based on the url of the window
-const SERVER_URL = window.location.href.includes("sail.cs.illinois.edu") ? PROD_SERVER : TEST_SERVER;
-
-export { PROD_SERVER, TEST_SERVER };
-export default SERVER_URL;
