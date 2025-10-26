245ffb7d0ff618597d4ce285f80e2a6bcad87e15
tiakashyap <tia.kashyap.2005@gmail.com>
2025-10-26 18:47:02 -0500

    removing hardcoding of server url

--- frontend/src/components/server_url.js
+++ frontend/src/components/server_url.js
@@ -1,8 +1,8 @@
 const PROD_SERVER = "https://sail.cs.illinois.edu";
-const TEST_SERVER = "http://10.192.104.68:5000";
+const BUILD_OVERRIDE = process.env.REACT_APP_SERVER_URL || null;
 
-// server URL based on the url of the window
-const SERVER_URL = window.location.href.includes("sail.cs.illinois.edu") ? PROD_SERVER : TEST_SERVER;
+// If BUILD_OVERRIDE is set (build-time), use it. Otherwise use the page origin.
+const SERVER_URL = BUILD_OVERRIDE || window.location.origin;
 
-export { PROD_SERVER, TEST_SERVER };
+export { PROD_SERVER };
 export default SERVER_URL;
