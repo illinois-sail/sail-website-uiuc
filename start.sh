ff7d31b917325e920f87260921c6411ccd05c0bf
tiakashyap <tia.kashyap.2005@gmail.com>
2025-10-26 19:20:52 -0500

    change requirements and port

--- start.sh
+++ start.sh
@@ -18,4 +18,4 @@ cd ../backend
 LOCAL_IP=$(python3 -c "import socket; s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM); s.connect(('1.1.1.1', 80)); print(s.getsockname()[0]); s.close()")
 export SERVER_URL="http://${LOCAL_IP}:5000"
 echo "[INFO] Using SERVER_URL=$SERVER_URL"
-python3 -m flask run --host 0.0.0.0
+python3 -m flask run --host 0.0.0.0 --port 8000
