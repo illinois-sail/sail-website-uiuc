adbf88231aad63286a1ea17b5c401f7fc3cfbcbe
seanpatel15 <spate758@illinois.edu>
2026-02-22 23:06:06 -0600

    initial profile

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const BusInformation = () => {
+  return (
+    <div className="bus-information" style={{
+      width: "83vw",
+      height: "auto",
+      border: "0.26vw solid #000",
+      background: "#FFF",
+    }}>
+      Hi there
+    </div>
+  );
+}
+
 function Profile() {
   return (
     <div>
-      <p style={{ color: "black" }}>Here goes the profile page!</p>
+      <BusInformation/>
     </div>
   );
 }
