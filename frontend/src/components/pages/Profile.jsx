6c4622f2553e8a076d91bdf488e4c2fdd15493e6
seanpatel15 <seanhpatel286@gmail.com>
2026-02-28 21:13:46 -0600

    started bus info

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Profile.css";
 
 const BusInformation = () => {
   return (
     <div className="bus-information" style={{
+      display: "flex",
+      marginTop: "5vw", // NOT FINAL
+      marginLeft: "7vw",
       width: "83vw",
-      height: "auto",
+      height: "44vw",
       border: "0.26vw solid #000",
-      background: "#FFF",
+      background: "#FFF"
     }}>
-      Hi there
+      <div className="notice-block" style={{ display: "flex", width: "17vw", height: "44vw", background: "#000", alignItems: "center" }}>
+        <span className="notice-text">Notice</span>
+      </div>
+      <div className="bus-info-body">
+        <div className="bus-info-header" style={{ marginTop: "3vw", marginLeft: "3vw" }}>
+          <span className="bus-info-title">Bus Information</span>
+        </div>
+      </div>
     </div>
   );
 }
