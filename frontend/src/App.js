622682b00aa9a06734e3c32e676eae79e80830ed
tiakashyap <tia.kashyap.2005@gmail.com>
2026-03-17 21:40:43 -0500

    reset password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/store";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./components/pages/Login";
@@ -11,38 +12,38 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Classes from "./components/pages/Classes";
 import Faq from "./components/pages/Faq";
-import SignUp from "./components/pages/SignUp"
+import SignUp from "./components/pages/SignUp";
 import Logout from "./components/pages/Logout";
 import ResetPassword from "./components/pages/ResetPassword";
+import ResetPasswordToken from "./components/pages/ResetPasswordToken";
 
 function App() {
   return (
-    <div className="app-layout">
-      <Navbar />
+    <Provider store={store}>
+      <div className="app-layout">
+        <Navbar />
 
-      <main className="page-content">
-        <Routes>
-          {/* Main pages */}
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/aboutus" element={<About />} />
-          <Route path="/classes" element={<Classes />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/transportation" element={<Transportation />} /> 
-          <Route path="/register" element={<Register />} />
+        <main className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/aboutus" element={<About />} />
+            <Route path="/classes" element={<Classes />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/transportation" element={<Transportation />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/reset_password" element={<ResetPassword />} />
+            <Route path="/reset_password/:token" element={<ResetPasswordToken />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </main>
 
-          {/* Auth pages */}
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/reset_password" element={<ResetPassword />} />
-          {/* <Route path="/profile" element={<Profile />} /> */}
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
