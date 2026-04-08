f2da6441910c5e2453510897196dd1fe83535aa6
seanpatel15 <seanhpatel286@gmail.com>
2026-04-08 16:52:39 -0500

    updated student schedule locations

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -159,7 +159,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">CIF</span>
+                    <span className="location-text">CIF 0027 - Auditorium</span>
                   </div>
                 </div>
               </div>
@@ -187,7 +187,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">Siebel Center for CS</span>
                   </div>
                 </div>
                 <div className="you-choose-block">
@@ -218,7 +218,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">Siebel Center for CS</span>
                   </div>
                 </div>
                 <div className="you-choose-block">
@@ -249,14 +249,14 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">Siebel Center: 2nd Floor Atrium (Lunch), 1404 (Panel)</span>
                   </div>
                   <div className="lunch-group-options-row">
                     <div className="lunch-group">
-                      <span className="group-text">GROUP A: Lunch</span>
+                      <span className="group-text">DC: Lunch</span>
                     </div>
                     <div className="panel-group">
-                      <span className="group-text">GROUP b: Q&A PANEL WITH cs students</span>
+                      <span className="group-text">Marvel: First Year Experience Panel</span>
                     </div>
                   </div>
                 </div>
@@ -285,14 +285,14 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">Siebel Center: 1404 (Panel), 2nd Floor Atrium (Lunch)</span>
                   </div>
                   <div className="lunch-group-options-row">
                     <div className="panel-group">
-                      <span className="group-text">GROUP A: Q&A PANEL WITH cs students</span>
+                      <span className="group-text">DC: First Year Experience Panel</span>
                     </div>
                     <div className="lunch-group">
-                      <span className="group-text">GROUP B: Lunch</span>
+                      <span className="group-text">Marvel: Lunch</span>
                     </div>
                   </div>
                 </div>
@@ -321,7 +321,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">Siebel Center for CS</span>
                   </div>
                 </div>
                 <div className="you-choose-block">
@@ -352,7 +352,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Campus Wide</span>
+                    <span className="location-text">Bardeen Engineering Quad</span>
                   </div>
                 </div>
               </div>
@@ -374,13 +374,13 @@ function Home() {
                     </svg>
                     <span className="time-text">4:00 PM</span>
                   </div>
-                  <span className="event-title">Professor Wade Talk</span>
+                  <span className="event-title">Professor Talk: Wade Fagen-Ulmschneider</span>
                   <div className="location-block">
                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">CIF</span>
+                    <span className="location-text">CIF 0027 - Auditorium</span>
                   </div>
                 </div>
               </div>
@@ -408,7 +408,7 @@ function Home() {
                       <path d="M10 5C10 7.4965 7.2305 10.0965 6.3005 10.8995C6.21386 10.9646 6.1084 10.9999 6 10.9999C5.8916 10.9999 5.78614 10.9646 5.6995 10.8995C4.7695 10.0965 2 7.4965 2 5C2 3.93913 2.42143 2.92172 3.17157 2.17157C3.92172 1.42143 4.93913 1 6 1C7.06087 1 8.07828 1.42143 8.82843 2.17157C9.57857 2.92172 10 3.93913 10 5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M6 6.5C6.82843 6.5 7.5 5.82843 7.5 5C7.5 4.17157 6.82843 3.5 6 3.5C5.17157 3.5 4.5 4.17157 4.5 5C4.5 5.82843 5.17157 6.5 6 6.5Z" stroke="#4A5565" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
-                    <span className="location-text">Siebel Center</span>
+                    <span className="location-text">CIF 0027 - Auditorium</span>
                   </div>
                 </div>
               </div>
